85443dbb402810b674e956033f5ea9bf401f9394
Aayush Sakpal <ayushSakpal007@gmail.com>
2024-12-06 12:21:56 +0530

    basic comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,12 @@
+console.log(2 > 1); // true
+console.log(2 == 1); // false
+console.log(2 != 1); // true
+console.log(3 <= 7); // true
+console.log(7 < 6); // false
+
+/*
+    if want to compare two values having different datatypes,
+    it is recommended to use conversion
+*/
+
+// assignmennt operators and comparision operators have edifferent working
